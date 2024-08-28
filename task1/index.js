b28ff5b522cab0164bb27ad0e70a49e950afcfcd
SATYAJIT BHUNIA <mesatya2000@gmail.com>
2024-08-28 12:06:17 +0530

    complete task-1

---
+++ task1/index.js
@@ -0,0 +1,80 @@
+const menuButton = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = document.querySelector("i");
+
+menuButton.addEventListener("click", (e) => {
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-menu-line" : "ri-close-line");
+  if (isOpen) {
+    navLinks.classList.add("close");
+    navLinks.addEventListener(
+      "animationend",
+      (e) => {
+        navLinks.classList.remove("open");
+        navLinks.classList.remove("close");
+      },
+      { once: true }
+    );
+  } else {
+    navLinks.classList.add("open");
+  }
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".header_container .section_subheader", {
+  ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".header_container .section_header", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".header_container .scrollBtn", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".header_container .header_socials", {
+  ...scrollRevealOption,
+  origin: "left",
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".about_image-1 .about_image-3", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".about_image-2", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".about_content .section_subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".about_content .section_header", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+
+ScrollReveal().reveal(".about_content p", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+ScrollReveal().reveal(".about_content .about_btn", {
+  ...scrollRevealOption,
+  delay: 2000,
+});
